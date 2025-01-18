feedab54d9ef82af215afd9b8f78881524dd4498
UBONGO2000 <gadyubongo@gmail.com>
2025-01-18 14:21:40 +0100

    first commit

---
+++ src/test/java/dev/georges/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.georges.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

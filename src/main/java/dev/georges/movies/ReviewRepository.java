47edb4e4decb818bf15437481937a05177c892e4
UBONGO2000 <gadyubongo@gmail.com>
2025-01-27 07:16:26 +0100

    creation des controller , repository et service de Movie et Review

---
+++ src/main/java/dev/georges/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.georges.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
